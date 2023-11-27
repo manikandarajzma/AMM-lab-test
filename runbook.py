08f32b104c9e8b48f7677798937e534b36a2e7c4
Mani Govindarajan <mani.govindarajan@aus01plpt201.local>
2023-11-27 11:45:08 -0600

    hostname config push works

--- runbook.py
+++ runbook.py
@@ -7,7 +7,7 @@ from nornir_utils.plugins.tasks.data import load_yaml
 
 
 nr = InitNornir(config_file="config.yaml")
-ios_devices = nr.filter(nr.host['platform']== 'ios')
+
 
 def load_variables(task):
     data = task.run(task=load_yaml, file=f"variables/{ task.host }.yaml")
@@ -17,8 +17,8 @@ def load_variables(task):
 
 def random_config(task):
     template = task.run(task=template_file, template="config.j2",path=f"templates/{task.host.platform}/")
-    task.host["ospf_config"] = template.result
-    rendered = task.host["ospf_config"]
+    task.host["run_config"] = template.result
+    rendered = task.host["run_config"]
     configuration = rendered.splitlines()
     task.run(task=netmiko_send_config, config_commands=configuration)
 
@@ -26,5 +26,5 @@ def random_config(task):
     # task.run(task=netmiko_send_command, command_string= 'no username lauren')
     # task.run(task=netmiko_send_command, command_string= '\n',expect_string = r"('This operation will remove all username related configurations with same name.Do you want to continue? [confirm]')")
 
-results = ios_devices.run(task=load_variables)
+results = nr.run(task=load_variables)
 print_result(results)
